c27320ceb5aee249ca10b61c54a7df569ffd4f84
DevSrSouza <devsrsouza@gmail.com>
2018-08-07 12:10:05 -0300

    serializers

---
+++ src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/dsl/config/Serializers.kt
@@ -0,0 +1,79 @@
+package br.com.devsrsouza.kotlinbukkitapi.dsl.config
+
+import br.com.devsrsouza.kotlinbukkitapi.dsl.item.fromBase64Item
+import br.com.devsrsouza.kotlinbukkitapi.dsl.item.toBase64
+import br.com.devsrsouza.kotlinbukkitapi.extensions.text.toJson
+import br.com.devsrsouza.kotlinbukkitapi.extensions.text.unaryMinus
+import br.com.devsrsouza.kotlinbukkitapi.extensions.text.unaryPlus
+import br.com.devsrsouza.kotlinbukkitapi.utils.whenErrorNull
+import net.md_5.bungee.api.chat.BaseComponent
+import net.md_5.bungee.api.chat.TextComponent
+import net.md_5.bungee.chat.ComponentSerializer
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.inventory.ItemStack
+
+fun locationSerializer(location: Location, description: String = "")
+        = Serializable(location, description).apply {
+    load {
+        (it as? String)?.split(";")?.run {
+            val world = getOrNull(0)?.let { Bukkit.getWorld(it) }
+            val x = getOrNull(1)?.toDoubleOrNull()
+            val y = getOrNull(2)?.toDoubleOrNull()
+            val z = getOrNull(3)?.toDoubleOrNull()
+            val yaw = getOrNull(4)?.toFloatOrNull()
+            val pitch = getOrNull(5)?.toFloatOrNull()
+            if (world != null && x != null && y != null
+                    && z != null) {
+                Location(world, x, y, z,
+                        yaw ?: 0F,
+                        pitch ?: 0F)
+            } else null
+        } ?: default
+    }
+
+    save { "${world.name};$x;$y;$z;$yaw;$pitch" }
+}
+
+fun locationListSerializer(locations: MutableList<Location>, description: String = "")
+        = Serializable(locations, description).apply {
+    load {
+        (it as? List<String>)?.mapNotNull {
+            it.split(";").run {
+                val world = getOrNull(0)?.let { Bukkit.getWorld(it) }
+                val x = getOrNull(1)?.toDoubleOrNull()
+                val y = getOrNull(2)?.toDoubleOrNull()
+                val z = getOrNull(3)?.toDoubleOrNull()
+                val yaw = getOrNull(4)?.toFloatOrNull()
+                val pitch = getOrNull(5)?.toFloatOrNull()
+                if(world != null && x != null && y != null
+                        && z != null) {
+                    Location(world, x, y, z, yaw ?: 0F, pitch ?: 0F)
+                } else null
+            }
+        }?.toMutableList() ?: default
+    }
+    save { map { "${it.world.name};${it.x};${it.y};${it.z};${it.yaw};${it.pitch}" } }
+}
+
+fun baseComponentSerializer(component: BaseComponent, description: String = "")
+        = Serializable(component, description).apply {
+    load { TextComponent(*ComponentSerializer.parse(+(it.toString()))) }
+    save { -toJson() }
+}
+
+fun itemBase64Serializer(item: ItemStack, description: String = "")
+        = Serializable(item, description).apply {
+    load { (it as? String)?.let { fromBase64Item(it) } ?: default }
+    save { toBase64() }
+}
+
+fun itemBase64ListSerializer(items: MutableList<ItemStack>, description: String = "")
+        = Serializable(items, description).apply {
+    load {
+        (it as? List<String>)?.mapNotNull {
+            whenErrorNull { fromBase64Item(it) }
+        }?.toMutableList() ?: default
+    }
+    save { map { it.toBase64() } }
+}
