58feb572a8b0cc8e63fb3a7ed22d5430e9eada1e
DevSrSouza <devsrsouza@gmail.com>
2018-08-28 11:43:14 -0300

    sendMessage and BaseComponent support

--- src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/plugins/KPlaceholderAPI.kt
+++ src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/plugins/KPlaceholderAPI.kt
@@ -1,6 +1,10 @@
 package br.com.devsrsouza.kotlinbukkitapi.plugins.placeholderapi
 
+import br.com.devsrsouza.kotlinbukkitapi.extensions.text.click
 import me.clip.placeholderapi.PlaceholderAPI
+import net.md_5.bungee.api.chat.BaseComponent
+import net.md_5.bungee.api.chat.ClickEvent
+import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import java.util.regex.Pattern
@@ -14,4 +18,28 @@ class KPlaceholderAPI(val player: Player) {
     fun setPlaceholders(message: String, pattern: Pattern) = PlaceholderAPI.setPlaceholders(player, message, pattern)
     fun setPlaceholders(messages: List<String>) = PlaceholderAPI.setPlaceholders(player, messages)
     fun setPlaceholders(messages: List<String>, pattern: Pattern) = PlaceholderAPI.setPlaceholders(player, messages, pattern)
+
+    fun sendMessage(message: String) = player.sendMessage(setPlaceholders(message))
+    fun sendMessage(message: List<String>) = setPlaceholders(message).forEach { player.sendMessage(it) }
+
+    fun <T : BaseComponent> setPlaceholders(component: T): T {
+        if (component is TextComponent)
+            component.text = setPlaceholders(component.text)
+
+        if (component.clickEvent != null)
+            component.click(ClickEvent(component.clickEvent.action, setPlaceholders(component.clickEvent.value)))
+
+        if (component.hoverEvent != null)
+            for (innerComponent in component.hoverEvent.value)
+                if (innerComponent is TextComponent) setPlaceholders(innerComponent)
+
+        if (component.extra != null)
+            for (innerComponent in component.extra)
+                setPlaceholders(innerComponent)
+
+        return component
+    }
+
+    fun sendMessage(text: BaseComponent) = player.spigot().sendMessage(setPlaceholders(text))
+    fun sendMessage(text: Array<BaseComponent>) = sendMessage(TextComponent(*text))
 }
