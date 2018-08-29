a8facc81f5dcbdab134cac8bf4fb1bc4a213639c
DevSrSouza <devsrsouza@gmail.com>
2018-08-29 10:28:17 -0300

    method to get player head from base64

--- src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/dsl/ItemStackDSL.kt
+++ src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/dsl/ItemStackDSL.kt
@@ -9,8 +9,10 @@ import java.util.*
 import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder
 import java.io.ByteArrayOutputStream
 import com.comphenix.attribute.NbtFactory.StreamOptions
+import com.google.common.collect.Multimap
 import java.io.ByteArrayInputStream
 import org.bukkit.Material
+import org.bukkit.inventory.meta.SkullMeta
 
 inline fun <T : ItemMeta> ItemStack.meta(block: T.() -> Unit) = apply {
     itemMeta = (itemMeta as T).apply(block)
@@ -71,4 +73,27 @@ fun fromBase64Item(data: String): ItemStack {
     }
 
     return stack
+}
+
+/**
+ * get head from base64
+ */
+val gameProfileClass by lazy { Class.forName("com.mojang.authlib.GameProfile") }
+val propertyClass by lazy { Class.forName("com.mojang.authlib.properties.Property") }
+val getPropertiesMethod by lazy { gameProfileClass.getMethod("getProperties") }
+val gameProfileConstructor by lazy { gameProfileClass.getConstructor(UUID::class.java, String::class.java) }
+val propertyConstructor by lazy { propertyClass.getConstructor(String::class.java, String::class.java) }
+
+fun headFromBase64(base64: String): ItemStack {
+    val item = ItemStack(Material.SKULL_ITEM, 1, 3)
+    val meta = item.itemMeta as SkullMeta
+
+    val profile = gameProfileConstructor.newInstance(UUID.randomUUID(), null as String?)
+    val properties = getPropertiesMethod.invoke(profile) as Multimap<Any, Any>
+    properties.put("textures", propertyConstructor.newInstance("textures", base64))
+
+    val profileField = meta.javaClass.getDeclaredField("profile").apply { isAccessible = true }
+    profileField.set(meta, profile)
+
+    return item.apply { itemMeta = meta }
 }
