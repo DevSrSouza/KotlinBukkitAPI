f1652fa06f98e8d2409ef470f452a530b6df9dc4
DevSrSouza <devsrsouza@gmail.com>
2018-10-06 14:00:19 -0300

    new and changes on RandomUtils

--- src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/utils/RandomUtils.kt
+++ src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/utils/RandomUtils.kt
@@ -5,4 +5,18 @@ import java.util.*
 private val rand by lazy { Random() }
 
 fun ClosedRange<Int>.random() = rand.nextInt(endInclusive - start) +  start
-fun <E> List<E>.random(): E? = if (size > 0) get(rand.nextInt(size)) else null
+fun <E> List<E>.randomIndex(): Int = if(size > 0) rand.nextInt(size) else -1
+fun <E> List<E>.random(): E = if(size > 0) get(rand.nextInt(size)) else throw IndexOutOfBoundsException()
+fun <E> List<E>.randomOrNull(): E? = if(size > 0) get(rand.nextInt(size)) else null
+fun <E> List<E>.randomize(): MutableList<E> {
+    val oldList = toMutableList()
+    val newList = mutableListOf<E>()
+
+    for(i in 0 until size) {
+        val index = oldList.randomIndex()
+        newList.add(oldList.get(index))
+        oldList.removeAt(index)
+    }
+
+    return newList
+}
