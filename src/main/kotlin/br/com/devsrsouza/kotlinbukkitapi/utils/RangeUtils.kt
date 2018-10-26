5f24eed8abf2ce12c38340a2d725da68b4a59727
DevSrSouza <devsrsouza@gmail.com>
2018-10-26 15:05:15 -0300

    better location range

--- src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/utils/RangeUtils.kt
+++ src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/utils/RangeUtils.kt
@@ -1,6 +1,7 @@
 package br.com.devsrsouza.kotlinbukkitapi.utils
 
 import org.bukkit.Location
+import org.bukkit.World
 import kotlin.math.sqrt
 
 data class SimpleLocation(val x: Int, val y: Int, val z: Int) : Comparable<SimpleLocation> {
@@ -12,10 +13,23 @@ data class SimpleLocation(val x: Int, val y: Int, val z: Int) : Comparable<Simpl
     }
 }
 
-class SimpleLocationRange(val first: SimpleLocation, val last: SimpleLocation) : ClosedRange<SimpleLocation>, Iterator<SimpleLocation> {
+class SimpleLocationRange<T>(val first: SimpleLocation, val last: SimpleLocation, val iterator: Iterator<T>)
+    : ClosedRange<SimpleLocation>, Iterator<T> {
+
     override val endInclusive: SimpleLocation get() = last
     override val start: SimpleLocation get() = first
 
+    override fun contains(value: SimpleLocation): Boolean {
+        return value.x >= first.x && value.x <= last.x
+                && value.y >= first.y && value.y <= last.y
+                && value.z >= first.z && value.z <= last.z
+    }
+
+    override fun hasNext() = iterator.hasNext()
+    override fun next() = iterator.next()
+}
+
+class SimpleLocationRangeIterator(first: SimpleLocation, last: SimpleLocation) : Iterator<SimpleLocation> {
     private val closedRangeX = IntProgression.fromClosedRange(first.x, last.x, 1)
     private val closedRangeY = IntProgression.fromClosedRange(first.y, last.y, 1)
     private val closedRangeZ = IntProgression.fromClosedRange(first.z, last.z, 1)
@@ -30,12 +44,6 @@ class SimpleLocationRange(val first: SimpleLocation, val last: SimpleLocation) :
     private inline fun iteratorY() = closedRangeY.iterator()
     private inline fun iteratorZ() = closedRangeZ.iterator()
 
-    override fun contains(value: SimpleLocation): Boolean {
-        return value.x >= first.x && value.x <= last.x
-                && value.y >= first.y && value.y <= last.y
-                && value.z >= first.z && value.z <= last.z
-    }
-
     override fun hasNext(): Boolean {
         if (iteratorX.hasNext()) return true
         else if (iteratorY.hasNext()) return true
@@ -44,14 +52,14 @@ class SimpleLocationRange(val first: SimpleLocation, val last: SimpleLocation) :
     }
 
     override fun next(): SimpleLocation {
-        if(iteratorZ.hasNext()) {
+        if (iteratorZ.hasNext()) {
             return SimpleLocation(actualX, actualY, iteratorZ.nextInt())
         } else {
-            if(iteratorY.hasNext()) {
+            if (iteratorY.hasNext()) {
                 actualY = iteratorY.nextInt()
                 iteratorZ = iteratorZ()
             } else {
-                if(iteratorX.hasNext()) {
+                if (iteratorX.hasNext()) {
                     actualX = iteratorX.nextInt()
                     iteratorY = iteratorY()
                 }
@@ -61,14 +69,24 @@ class SimpleLocationRange(val first: SimpleLocation, val last: SimpleLocation) :
     }
 }
 
-operator fun SimpleLocation.rangeTo(other: SimpleLocation): SimpleLocationRange {
-    return SimpleLocationRange(this, other)
+operator fun SimpleLocation.rangeTo(other: SimpleLocation): SimpleLocationRange<SimpleLocation> {
+    return SimpleLocationRange(this, other, SimpleLocationRangeIterator(this, other))
 }
 
 // BUKKIT THINGS
 
-inline fun Location.asSimple() = SimpleLocation(blockX, blockY, blockZ)
+operator fun SimpleLocationRange<*>.contains(other: Location) = contains(other.asSimple())
 
-operator fun Location.rangeTo(other: Location): SimpleLocationRange {
-    return asSimple()..other.asSimple()
+inline fun Location.asSimple() = SimpleLocation(blockX, blockY, blockZ)
+inline fun SimpleLocation.asBukkitLocation(world: World) = Location(world, x.toDouble(), y.toDouble(), z.toDouble())
+
+class LocationRangeIterator(val start: Location, val end: Location) : Iterator<Location> {
+    val iterator = SimpleLocationRangeIterator(start.asSimple(), end.asSimple())
+
+    override fun hasNext() = iterator.hasNext()
+    override fun next() = iterator.next().asBukkitLocation(start.world)
+}
+
+operator fun Location.rangeTo(other: Location): SimpleLocationRange<Location> {
+    return SimpleLocationRange(this.asSimple(), other.asSimple(), LocationRangeIterator(this, other))
 }
