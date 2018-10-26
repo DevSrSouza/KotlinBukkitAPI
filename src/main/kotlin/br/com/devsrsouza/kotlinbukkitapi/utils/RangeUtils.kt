19e614eb75b5e110c5a02e85021f966390ea03bf
DevSrSouza <devsrsouza@gmail.com>
2018-10-25 21:49:28 -0300

    fixing contains of location range not working well

--- src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/utils/RangeUtils.kt
+++ src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/utils/RangeUtils.kt
@@ -1,16 +1,14 @@
 package br.com.devsrsouza.kotlinbukkitapi.utils
 
 import org.bukkit.Location
+import kotlin.math.sqrt
 
 data class SimpleLocation(val x: Int, val y: Int, val z: Int) : Comparable<SimpleLocation> {
     override fun compareTo(other: SimpleLocation): Int {
-        val cx = x.compareTo(other.x)
-        if(cx == 0) {
-            val cz = z.compareTo(other.z)
-            if(cz == 0) {
-                return y.compareTo(other.y)
-            } else return cz
-        } else return cx
+        val d1 = sqrt((x*x + y*y + z*z).toDouble())
+        val d2 = other.run { sqrt((x*x + y*y + z*z).toDouble()) }
+
+        return d1.compareTo(d2)
     }
 }
 
@@ -31,6 +29,12 @@ class SimpleLocationRange(val first: SimpleLocation, val last: SimpleLocation) :
 
     private inline fun iteratorY() = closedRangeY.iterator()
     private inline fun iteratorZ() = closedRangeZ.iterator()
+
+    override fun contains(value: SimpleLocation): Boolean {
+        return value.x >= first.x && value.x <= last.x
+                && value.y >= first.y && value.y <= last.y
+                && value.z >= first.z && value.z <= last.z
+    }
 
     override fun hasNext(): Boolean {
         if (iteratorX.hasNext()) return true
