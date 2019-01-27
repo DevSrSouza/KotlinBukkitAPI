6346028767d448607ff7a41ec44d33f89ec788e7
DevSrSouza <devsrsouza@gmail.com>
2019-01-27 12:48:07 -0200

    checker if entity is player with auto cast using contract

--- core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/KEntity.kt
+++ core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/KEntity.kt
@@ -1,7 +1,17 @@
-package br.com.devsrsouza.kotlinbukkitapi.extensions
+package br.com.devsrsouza.kotlinbukkitapi.extensions.entity
 
+import org.bukkit.entity.Entity
+import org.bukkit.entity.EntityType
 import org.bukkit.entity.Firework
+import org.bukkit.entity.Player
 import org.bukkit.inventory.meta.FireworkMeta
+import kotlin.contracts.contract
+
+val Entity.isPlayer: Boolean
+    get() {
+        contract { returns(true) implies (this@isPlayer is Player) }
+        return type == EntityType.PLAYER
+    }
 
 inline fun Firework.meta(block: FireworkMeta.() -> Unit) = apply {
     fireworkMeta = fireworkMeta.apply(block)
