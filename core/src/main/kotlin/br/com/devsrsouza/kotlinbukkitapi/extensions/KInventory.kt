48222485b1b51415d3f2b98558643b476eb2f070
DevSrSouza <devsrsouza@gmail.com>
2019-03-09 01:55:38 -0300

    hasSpace for specific item

--- core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/KInventory.kt
+++ core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/KInventory.kt
@@ -2,6 +2,22 @@ package br.com.devsrsouza.kotlinbukkitapi.extensions.inventory
 
 import org.bukkit.Material
 import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
 
 val Inventory.hasSpace: Boolean
     get() = contents.any { it == null || it.type == Material.AIR }
+
+fun Inventory.hasSpace(
+        item: ItemStack,
+        amount: Int = item.amount
+) = spaceOf(item) >= amount
+
+fun Inventory.spaceOf(
+        item: ItemStack
+): Int {
+    return contents.filterNotNull().map {
+        if (it.amount < it.maxStackSize && it.isSimilar(item))
+            it.maxStackSize - it.amount
+        else 0
+    }.count()
+}
