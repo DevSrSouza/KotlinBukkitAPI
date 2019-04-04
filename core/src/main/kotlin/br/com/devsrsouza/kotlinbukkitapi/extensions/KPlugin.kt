10ff385065662744227ba53ba216647eee4cf8f9
DevSrSouza <devsrsouza@gmail.com>
2019-04-03 21:35:36 -0300

    logging for WithPlugin

--- core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/KPlugin.kt
+++ core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/KPlugin.kt
@@ -17,4 +17,9 @@ fun Plugin.warn(message: String) = logger.warning(message)
 fun Plugin.severe(message: String) = logger.severe(message)
 fun Plugin.fine(message: String) = logger.fine(message)
 
+fun WithPlugin<*>.info(message: String) = plugin.info(message)
+fun WithPlugin<*>.warn(message: String) = plugin.warn(message)
+fun WithPlugin<*>.severe(message: String) = plugin.severe(message)
+fun WithPlugin<*>.fine(message: String) = plugin.fine(message)
+
 interface WithPlugin<T : Plugin> { val plugin: T }
