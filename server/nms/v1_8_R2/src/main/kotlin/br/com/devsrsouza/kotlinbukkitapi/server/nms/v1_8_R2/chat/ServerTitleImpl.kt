d7404d89d31f86bb40ecc3fd6db3be999c5431e0
DevSrSouza <devsrsouza@gmail.com>
2019-04-13 12:31:26 -0300

    all 1.8 version nms support

---
+++ server/nms/v1_8_R2/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/server/nms/v1_8_R2/chat/ServerTitleImpl.kt
@@ -0,0 +1,40 @@
+package br.com.devsrsouza.kotlinbukkitapi.server.nms.v1_8_R2.chat
+
+import br.com.devsrsouza.kotlinbukkitapi.server.api.chat.ServerTitle
+import br.com.devsrsouza.kotlinbukkitapi.server.nms.v1_8_R2.asIChatBaseComponent
+import br.com.devsrsouza.kotlinbukkitapi.server.nms.v1_8_R2.sendPacket
+import net.md_5.bungee.api.chat.TextComponent
+import net.minecraft.server.v1_8_R2.PacketPlayOutTitle
+import org.bukkit.entity.Player
+
+object ServerTitleImpl : ServerTitle {
+
+    private fun sendAnyTitle(player: Player, action: PacketPlayOutTitle.EnumTitleAction, title: String) {
+        val component = TextComponent.fromLegacyText(title).asIChatBaseComponent()
+
+        val packet = PacketPlayOutTitle(action, component)
+
+        player.sendPacket(packet)
+    }
+
+    override fun sendTitle(player: Player, title: String) {
+        sendAnyTitle(player, PacketPlayOutTitle.EnumTitleAction.TITLE, title)
+    }
+
+    override fun sendSubtitle(player: Player, subtitle: String) {
+        sendAnyTitle(player, PacketPlayOutTitle.EnumTitleAction.SUBTITLE, subtitle)
+    }
+
+    override fun sendTitleTime(player: Player, fadeIn: Int, stay: Int, fadeOut: Int) {
+        val packet = PacketPlayOutTitle(fadeIn, stay, fadeOut)
+
+        player.sendPacket(packet)
+    }
+
+    override fun resetTitle(player: Player) {
+        val packet = PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.RESET, null)
+
+        player.sendPacket(packet)
+    }
+
+}
---
+++ server/nms/v1_8_R2/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/server/nms/v1_8_R2/nbt/ServerNBTImpl.kt
@@ -0,0 +1,73 @@
+package br.com.devsrsouza.kotlinbukkitapi.server.nms.v1_8_R2.nbt
+
+import br.com.devsrsouza.kotlinbukkitapi.server.api.nbt.ServerNBT
+import br.com.devsrsouza.kotlinnbt.api.ITag
+import br.com.devsrsouza.kotlinnbt.api.TagType
+import br.com.devsrsouza.kotlinnbt.api.tags.*
+import net.minecraft.server.v1_8_R2.*
+import java.lang.IllegalArgumentException
+import kotlin.reflect.KClass
+
+object ServerNBTImpl : ServerNBT<NBTTagCompound, NBTBase> {
+    val mapField by lazy { NBTTagCompound::class.java.getDeclaredField("map") }
+    
+    fun NBTTagCompound.getMap(): Map<String, NBTBase> {
+        return mapField.get(this) as Map<String, NBTBase>
+    }
+
+    override fun serverTagAsKotlinNBTTag(tag: NBTBase): ITag {
+        return when(tag) {
+            is NBTTagString -> StringTag(tag.a_())
+            is NBTTagDouble -> DoubleTag(tag.g())
+            is NBTTagFloat -> FloatTag(tag.h())
+            is NBTTagLong -> LongTag(tag.c())
+            is NBTTagInt -> IntTag(tag.d())
+            is NBTTagShort -> ShortTag(tag.e())
+            is NBTTagByte -> ByteTag(tag.f())
+            is NBTTagByteArray -> ByteArrayTag(tag.c())
+            is NBTTagIntArray -> IntArrayTag(tag.c())
+            is NBTTagCompound -> compoundAsKotlinNBT(tag)
+            is NBTTagList -> {
+                val typeId = if(tag.isEmpty) TagType.END else TagType.byID(tag.g(0).getTypeId().toInt())!!
+
+                return ServerListTag(typeId.clazz as KClass<ITag>, tag)
+            }
+            else -> throw IllegalArgumentException("NBT tag not support on KotlinNBT")
+        }
+    }
+
+    override fun kotlinNBTTagAsServerTag(tag: ITag): NBTBase {
+        return when(tag) {
+            is StringTag -> NBTTagString(tag.value)
+            is DoubleTag -> NBTTagDouble(tag.value)
+            is FloatTag -> NBTTagFloat(tag.value)
+            is LongTag -> NBTTagLong(tag.value)
+            is IntTag -> NBTTagInt(tag.value)
+            is ShortTag -> NBTTagShort(tag.value)
+            is ByteTag -> NBTTagByte(tag.value)
+            is ByteArrayTag -> NBTTagByteArray(tag.value)
+            is IntArrayTag -> NBTTagIntArray(tag.value)
+            is CompoundTag -> kotlinNBTAsCompound(tag)
+            is ListTag<*> -> NBTTagList().also { list ->
+                tag.forEach { list.add(kotlinNBTTagAsServerTag(it)) }
+            }
+            else -> throw IllegalArgumentException("tag ${tag.type} is not supported in this version.")
+        }
+    }
+
+    override fun compoundAsKotlinNBT(nbt: NBTTagCompound): CompoundTag {
+        return ServerCompoundTag(nbt)
+    }
+
+    override fun kotlinNBTAsCompound(compound: CompoundTag): NBTTagCompound {
+        val nbt = NBTTagCompound()
+
+        for (tag in compound) {
+            runCatching { kotlinNBTTagAsServerTag(tag.value) }.getOrNull()?.also {
+                nbt.set(tag.key, it)
+            }
+        }
+
+        return nbt
+    }
+}
