aba522994ce47e6c0ee2748abb7a415560d527fd
DevSrSouza <devsrsouza@gmail.com>
2019-04-25 13:42:39 -0300

    removing skedule because is not the scope of the project anymore

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,12 +1,7 @@
 repositories {
-    maven {
-        name = "okkero"
-        url = uri("http://nexus.okkero.com/repository/maven-releases/")
-    }
 }
 
 dependencies {
-    compile("com.okkero.skedule:skedule:1.2.5")
     compile("br.com.devsrsouza:config4bukkit:1.0.0-SNAPSHOT")
     compile("com.typesafe:config:1.3.2")
 }
--- core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/KScheduler.kt
+++ core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/KScheduler.kt
@@ -1,47 +1,57 @@
 package br.com.devsrsouza.kotlinbukkitapi.extensions.scheduler
 
-import br.com.devsrsouza.kotlinbukkitapi.KotlinBukkitAPI
 import br.com.devsrsouza.kotlinbukkitapi.extensions.plugin.WithPlugin
-import com.okkero.skedule.BukkitSchedulerController
-import com.okkero.skedule.CoroutineTask
-import com.okkero.skedule.SynchronizationContext
-import com.okkero.skedule.schedule
 import org.bukkit.plugin.Plugin
 import org.bukkit.scheduler.BukkitRunnable
 
-inline fun task(delayToRun: Long = 0,
-                plugin: Plugin,
-                crossinline controller: suspend BukkitSchedulerController.() -> Unit
-): CoroutineTask = task(delayToRun, false, plugin, controller)
+inline fun task(
+        delayToRun: Long = 0,
+        repeatDelay: Long = -1,
+        plugin: Plugin,
+        crossinline runnable: BukkitRunnable.() -> Unit
+) = task(delayToRun, repeatDelay, false, plugin, runnable)
 
-inline fun Plugin.task(delayToRun: Long = 0,
-                       crossinline controller: suspend BukkitSchedulerController.() -> Unit
-): CoroutineTask = task(delayToRun, this, controller)
+inline fun Plugin.task(
+        delayToRun: Long = 0,
+        repeatDelay: Long = -1,
+        crossinline runnable: BukkitRunnable.() -> Unit
+) = task(delayToRun, repeatDelay, this, runnable)
 
-inline fun WithPlugin<*>.task(delayToRun: Long = 0,
-                       crossinline controller: suspend BukkitSchedulerController.() -> Unit
-): CoroutineTask = plugin.task(delayToRun, controller)
+inline fun WithPlugin<*>.task(
+        delayToRun: Long = 0,
+        repeatDelay: Long = -1,
+        crossinline runnable: BukkitRunnable.() -> Unit
+) = plugin.task(delayToRun, repeatDelay, runnable)
 
-inline fun taskAsync(delayToRun: Long = 0,
-                     plugin: Plugin,
-                     crossinline controller: suspend BukkitSchedulerController.() -> Unit
-): CoroutineTask = task(delayToRun, true, plugin, controller)
+inline fun taskAsync(
+        delayToRun: Long = 0,
+        repeatDelay: Long = -1,
+        plugin: Plugin,
+        crossinline runnable: BukkitRunnable.() -> Unit
+) = task(delayToRun, repeatDelay, true, plugin, runnable)
 
-inline fun Plugin.taskAsync(delayToRun: Long = 0,
-                            crossinline controller: suspend BukkitSchedulerController.() -> Unit
-): CoroutineTask = taskAsync(delayToRun, this, controller)
+inline fun Plugin.taskAsync(
+        delayToRun: Long = 0,
+        repeatDelay: Long = -1,
+        crossinline runnable: BukkitRunnable.() -> Unit
+) = taskAsync(delayToRun, repeatDelay, this, runnable)
 
-inline fun WithPlugin<*>.taskAsync(delayToRun: Long = 0,
-                            crossinline controller: suspend BukkitSchedulerController.() -> Unit
-): CoroutineTask = plugin.taskAsync(delayToRun, controller)
+inline fun WithPlugin<*>.taskAsync(
+        delayToRun: Long = 0,
+        repeatDelay: Long = -1,
+        crossinline runnable: BukkitRunnable.() -> Unit
+) = plugin.taskAsync(delayToRun, repeatDelay, runnable)
 
-inline fun task(delayToRun: Long,
-                async: Boolean,
-                plugin: Plugin = KotlinBukkitAPI.INSTANCE,
-                crossinline controller: suspend BukkitSchedulerController.() -> Unit
-): CoroutineTask = plugin.schedule(if (async) SynchronizationContext.ASYNC else SynchronizationContext.SYNC) {
-    if (delayToRun > 0) waitFor(delayToRun)
-    controller()
+inline fun task(
+        delayToRun: Long,
+        repeatDelay: Long = -1,
+        async: Boolean,
+        plugin: Plugin,
+        crossinline runnable: BukkitRunnable.() -> Unit
+) = scheduler(runnable).run {
+    if (repeatDelay > -1) if (async) runTaskTimerAsynchronously(plugin, repeatDelay, delayToRun) else runTaskTimer(plugin, repeatDelay, delayToRun)
+    else if (delayToRun > 0) if (async) runTaskLaterAsynchronously(plugin, delayToRun) else runTaskLater(plugin, delayToRun)
+    else if (async) runTaskAsynchronously(plugin) else runTask(plugin)
 }
 
 inline fun scheduler(crossinline runnable: BukkitRunnable.() -> Unit) = object : BukkitRunnable() {
