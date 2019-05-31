2042d6a9a47e56a335184c7f2b21aab456f38c32
DevSrSouza <devsrsouza@gmail.com>
2019-05-31 12:07:44 -0300

    BlockPos and LocationPos delegate for Exposed DAO

---
+++ exposed/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/exposed/BlockPos.kt
@@ -0,0 +1,139 @@
+package br.com.devsrsouza.kotlinbukkitapi.exposed
+
+import br.com.devsrsouza.kotlinbukkitapi.utils.BlockPos
+import org.jetbrains.exposed.dao.Entity
+import org.jetbrains.exposed.sql.Column
+import kotlin.reflect.KProperty
+
+fun Entity<*>.blockPos(column: Column<String>) = BlockPosExposedDelegate(column)
+fun Entity<*>.blockPos(column: Column<String?>) = BlockPosExposedDelegateNullable(column)
+
+fun Entity<*>.blockPos(
+        xColumn: Column<Int>,
+        yColumn: Column<Int>,
+        zColumn: Column<Int>
+) = BlockPosMultiColumnExposedDelegate(xColumn, yColumn, zColumn)
+
+fun Entity<*>.blockPos(
+        xColumn: Column<Int?>,
+        yColumn: Column<Int?>,
+        zColumn: Column<Int?>
+) = BlockPosMultiColumnExposedDelegateNullable(xColumn, yColumn, zColumn)
+
+class BlockPosExposedDelegate(
+        val column: Column<String>
+) : ExposedDelegate<BlockPos> {
+    override operator fun <ID : Comparable<ID>> getValue(
+            entity: Entity<ID>,
+            desc: KProperty<*>
+    ): BlockPos {
+        val data = entity.run { column.getValue(this, desc) }
+        val slices = data.split(";")
+        return BlockPos(
+                slices[0].toInt(),
+                slices[1].toInt(),
+                slices[2].toInt()
+        )
+    }
+
+    override operator fun <ID : Comparable<ID>> setValue(
+            entity: Entity<ID>,
+            desc: KProperty<*>,
+            value: BlockPos
+    ) {
+        val parsed = value.run { "$x;$y;$z" }
+        entity.apply { column.setValue(this, desc, parsed) }
+    }
+}
+
+class BlockPosExposedDelegateNullable(
+        val column: Column<String?>
+) : ExposedDelegate<BlockPos?> {
+    override operator fun <ID : Comparable<ID>> getValue(
+            entity: Entity<ID>,
+            desc: KProperty<*>
+    ): BlockPos? {
+        val data = entity.run { column.getValue(this, desc) }
+        val slices = data?.split(";")
+        return slices?.let {
+            BlockPos(
+                    it[0].toInt(),
+                    it[1].toInt(),
+                    it[2].toInt()
+            )
+        }
+    }
+
+    override operator fun <ID : Comparable<ID>> setValue(
+            entity: Entity<ID>,
+            desc: KProperty<*>,
+            value: BlockPos?
+    ) {
+        val parsed = value?.run { "$x;$y;$z" }
+        entity.apply { column.setValue(this, desc, parsed) }
+    }
+}
+
+class BlockPosMultiColumnExposedDelegate(
+        val xColumn: Column<Int>,
+        val yColumn: Column<Int>,
+        val zColumn: Column<Int>
+) : ExposedDelegate<BlockPos> {
+    override operator fun <ID : Comparable<ID>> getValue(
+            entity: Entity<ID>,
+            desc: KProperty<*>
+    ): BlockPos {
+        val x = entity.run { xColumn.getValue(this, desc) }
+        val y = entity.run { yColumn.getValue(this, desc) }
+        val z = entity.run { zColumn.getValue(this, desc) }
+
+        return BlockPos(x, y, z)
+    }
+
+    override operator fun <ID : Comparable<ID>> setValue(
+            entity: Entity<ID>,
+            desc: KProperty<*>,
+            value: BlockPos
+    ) {
+        entity.apply {
+            value.apply {
+                xColumn.setValue(entity, desc, x)
+                yColumn.setValue(entity, desc, y)
+                zColumn.setValue(entity, desc, z)
+            }
+        }
+    }
+}
+
+class BlockPosMultiColumnExposedDelegateNullable(
+        val xColumn: Column<Int?>,
+        val yColumn: Column<Int?>,
+        val zColumn: Column<Int?>
+) : ExposedDelegate<BlockPos?> {
+    override operator fun <ID : Comparable<ID>> getValue(
+            entity: Entity<ID>,
+            desc: KProperty<*>
+    ): BlockPos? {
+        val x = entity.run { xColumn.getValue(this, desc) }
+        val y = entity.run { yColumn.getValue(this, desc) }
+        val z = entity.run { zColumn.getValue(this, desc) }
+
+        return if (
+                x != null && y != null && z != null
+        ) BlockPos(
+                x, y, z
+        ) else null
+    }
+
+    override operator fun <ID : Comparable<ID>> setValue(
+            entity: Entity<ID>,
+            desc: KProperty<*>,
+            value: BlockPos?
+    ) {
+        entity.apply {
+            xColumn.setValue(entity, desc, value?.x)
+            yColumn.setValue(entity, desc, value?.y)
+            zColumn.setValue(entity, desc, value?.z)
+        }
+    }
+}
---
+++ exposed/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/exposed/LocationPos.kt
@@ -0,0 +1,162 @@
+package br.com.devsrsouza.kotlinbukkitapi.exposed
+
+import br.com.devsrsouza.kotlinbukkitapi.utils.LocationPos
+import org.jetbrains.exposed.dao.Entity
+import org.jetbrains.exposed.sql.Column
+import kotlin.reflect.KProperty
+
+fun Entity<*>.locationPos(column: Column<String>) = LocationPosExposedDelegate(column)
+fun Entity<*>.locationPos(column: Column<String?>) = LocationPosExposedDelegateNullable(column)
+
+fun Entity<*>.locationPos(
+        xColumn: Column<Double>,
+        yColumn: Column<Double>,
+        zColumn: Column<Double>,
+        yawColumn: Column<Float>,
+        pitchColumn: Column<Float>
+) = LocationPosMultiColumnExposedDelegate(xColumn, yColumn, zColumn, yawColumn, pitchColumn)
+
+fun Entity<*>.locationPos(
+        xColumn: Column<Double?>,
+        yColumn: Column<Double?>,
+        zColumn: Column<Double?>,
+        yawColumn: Column<Float?>,
+        pitchColumn: Column<Float?>
+) = LocationPosMultiColumnExposedDelegateNullable(xColumn, yColumn, zColumn, yawColumn, pitchColumn)
+
+class LocationPosExposedDelegate(
+        val column: Column<String>
+) : ExposedDelegate<LocationPos> {
+    override operator fun <ID : Comparable<ID>> getValue(
+            entity: Entity<ID>,
+            desc: KProperty<*>
+    ): LocationPos {
+        val data = entity.run { column.getValue(this, desc) }
+        val slices = data.split(";")
+        return LocationPos(
+                slices[0].toDouble(),
+                slices[1].toDouble(),
+                slices[2].toDouble(),
+                slices[3].toFloat(),
+                slices[4].toFloat()
+        )
+    }
+
+    override operator fun <ID : Comparable<ID>> setValue(
+            entity: Entity<ID>,
+            desc: KProperty<*>,
+            value: LocationPos
+    ) {
+        val parsed = value.run { "$x;$y;$z;$yaw;$pitch" }
+        entity.apply { column.setValue(this, desc, parsed) }
+    }
+}
+
+class LocationPosExposedDelegateNullable(
+        val column: Column<String?>
+) : ExposedDelegate<LocationPos?> {
+    override operator fun <ID : Comparable<ID>> getValue(
+            entity: Entity<ID>,
+            desc: KProperty<*>
+    ): LocationPos? {
+        val data = entity.run { column.getValue(this, desc) }
+        val slices = data?.split(";")
+        return slices?.let {
+            LocationPos(
+                    it[0].toDouble(),
+                    it[1].toDouble(),
+                    it[2].toDouble(),
+                    it[3].toFloat(),
+                    it[4].toFloat()
+            )
+        }
+    }
+
+    override operator fun <ID : Comparable<ID>> setValue(
+            entity: Entity<ID>,
+            desc: KProperty<*>,
+            value: LocationPos?
+    ) {
+        val parsed = value?.run { "$x;$y;$z;$yaw;$pitch" }
+        entity.apply { column.setValue(this, desc, parsed) }
+    }
+}
+
+class LocationPosMultiColumnExposedDelegate(
+        val xColumn: Column<Double>,
+        val yColumn: Column<Double>,
+        val zColumn: Column<Double>,
+        val yawColumn: Column<Float>,
+        val pitchColumn: Column<Float>
+) : ExposedDelegate<LocationPos> {
+    override operator fun <ID : Comparable<ID>> getValue(
+            entity: Entity<ID>,
+            desc: KProperty<*>
+    ): LocationPos {
+        val x = entity.run { xColumn.getValue(this, desc) }
+        val y = entity.run { yColumn.getValue(this, desc) }
+        val z = entity.run { zColumn.getValue(this, desc) }
+        val yaw = entity.run { yawColumn.getValue(this, desc) }
+        val pitch = entity.run { pitchColumn.getValue(this, desc) }
+
+        return LocationPos(
+                x, y, z, yaw, pitch
+        )
+    }
+
+    override operator fun <ID : Comparable<ID>> setValue(
+            entity: Entity<ID>,
+            desc: KProperty<*>,
+            value: LocationPos
+    ) {
+        entity.apply {
+            value.apply {
+                xColumn.setValue(entity, desc, x)
+                yColumn.setValue(entity, desc, y)
+                zColumn.setValue(entity, desc, z)
+                yawColumn.setValue(entity, desc, yaw)
+                pitchColumn.setValue(entity, desc, pitch)
+            }
+        }
+    }
+}
+
+class LocationPosMultiColumnExposedDelegateNullable(
+        val xColumn: Column<Double?>,
+        val yColumn: Column<Double?>,
+        val zColumn: Column<Double?>,
+        val yawColumn: Column<Float?>,
+        val pitchColumn: Column<Float?>
+) : ExposedDelegate<LocationPos?> {
+    override operator fun <ID : Comparable<ID>> getValue(
+            entity: Entity<ID>,
+            desc: KProperty<*>
+    ): LocationPos? {
+        val x = entity.run { xColumn.getValue(this, desc) }
+        val y = entity.run { yColumn.getValue(this, desc) }
+        val z = entity.run { zColumn.getValue(this, desc) }
+        val yaw = entity.run { yawColumn.getValue(this, desc) }
+        val pitch = entity.run { pitchColumn.getValue(this, desc) }
+
+        return if (
+                x != null && y != null && z != null &&
+                yaw != null && pitch != null
+        ) LocationPos(
+                x, y, z, yaw, pitch
+        ) else null
+    }
+
+    override operator fun <ID : Comparable<ID>> setValue(
+            entity: Entity<ID>,
+            desc: KProperty<*>,
+            value: LocationPos?
+    ) {
+        entity.apply {
+            xColumn.setValue(entity, desc, value?.x)
+            yColumn.setValue(entity, desc, value?.y)
+            zColumn.setValue(entity, desc, value?.z)
+            yawColumn.setValue(entity, desc, value?.yaw)
+            pitchColumn.setValue(entity, desc, value?.pitch)
+        }
+    }
+}
