e27bd940cf5c572f2573c325b5f221a28c5905e4
DevSrSouza <devsrsouza@gmail.com>
2019-06-28 11:58:16 -0300

    Player extension, hasItemInHand

--- core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/ExPlayer.kt
+++ core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/ExPlayer.kt
@@ -1,6 +1,7 @@
 package br.com.devsrsouza.kotlinbukkitapi.extensions.player
 
 import org.bukkit.Instrument
+import org.bukkit.Material
 import org.bukkit.Note
 import org.bukkit.Sound
 import org.bukkit.entity.Player
@@ -15,6 +16,8 @@ fun PlayerInventory.clearAll() {
     clear()
     clearArmor()
 }
+
+val Player.hasItemInHand get() = itemInHand != null && itemInHand.type != Material.AIR
 
 fun Player.playSound(sound: Sound, volume: Float, pitch: Float) = playSound(location, sound, volume, pitch)
 fun Player.playNote(instrument: Instrument, note: Note) = playNote(location, instrument, note)
