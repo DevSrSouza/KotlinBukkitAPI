8c30a6e348b58da29f49b885c8bfb2b5f3ae1f1e
DevSrSouza <devsrsouza@gmail.com>
2019-07-05 11:46:06 -0300

    default load and save adapter

--- core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/config/PropertyAdapter.kt
+++ core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/config/PropertyAdapter.kt
@@ -1,10 +1,20 @@
 package br.com.devsrsouza.kotlinbukkitapi.config
 
+import br.com.devsrsouza.kotlinbukkitapi.config.adapter.chatColorLoadAdapter
+import br.com.devsrsouza.kotlinbukkitapi.config.adapter.chatColorSaveAdapter
+import br.com.devsrsouza.kotlinbukkitapi.config.adapter.parserParseAdapter
+import br.com.devsrsouza.kotlinbukkitapi.config.adapter.parserRenderAdapter
 import kotlin.reflect.KMutableProperty1
 
 typealias PropertyAdapter = (instance: Any, property: KMutableProperty1<Any, Any>, value: Any) -> Any
 
 fun emptyAdapter(): PropertyAdapter = { _, _, value -> value }
+
+fun defaultLoadAdapter(): PropertyAdapter = chatColorLoadAdapter()
+        .with(parserParseAdapter())
+
+fun defaultSaveAdapter(): PropertyAdapter = chatColorSaveAdapter()
+        .with(parserRenderAdapter())
 
 fun PropertyAdapter.with(adapter: PropertyAdapter): PropertyAdapter = { instance, property, value ->
     adapter(instance, property, invoke(instance, property, value))
