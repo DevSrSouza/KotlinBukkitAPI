39701e734972d467d3cebf02a0a354f1c133389e
DevSrSouza <devsrsouza@gmail.com>
2019-07-28 12:55:05 -0300

    getIgnoreCase for Map<String, T>

--- core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/utils/CollectionUtils.kt
+++ core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/utils/CollectionUtils.kt
@@ -2,12 +2,16 @@ package br.com.devsrsouza.kotlinbukkitapi.utils
 
 fun Collection<String>.containsIgnoreCase(
         element: String
-) = any { it.equals(element, true) }
+): Boolean = any { it.equals(element, true) }
 
 fun Array<String>.containsIgnoreCase(
         element: String
-) = any { it.equals(element, true) }
+): Boolean = any { it.equals(element, true) }
 
 fun <V> Map<String, V>.containsKeyIgnoreCase(
         key: String
-) = keys.containsIgnoreCase(key)
+): Boolean = keys.containsIgnoreCase(key)
+
+fun <V> Map<String, V>.getIgnoreCase(
+        key: String
+): V? = entries.find { it.key.equals(key, true) }?.value
