6c45ef8019b6fd84a035e22ac1cbc63dd98c9332
DevSrSouza <devsrsouza@gmail.com>
2019-08-03 01:33:58 -0300

    enum command argument

---
+++ core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/dsl/command/arguments/Enums.kt
@@ -0,0 +1,31 @@
+package br.com.devsrsouza.kotlinbukkitapi.dsl.command.arguments
+
+import br.com.devsrsouza.kotlinbukkitapi.dsl.command.Executor
+import br.com.devsrsouza.kotlinbukkitapi.dsl.command.exception
+import br.com.devsrsouza.kotlinbukkitapi.extensions.text.color
+import br.com.devsrsouza.kotlinbukkitapi.utils.getIgnoreCase
+import net.md_5.bungee.api.chat.BaseComponent
+import org.bukkit.ChatColor
+
+// enum
+val MISSING_ENUM_PARAMETER = "Missing an enum argument.".color(ChatColor.RED)
+val ENUM_VALUE_NOT_FOUND = "The value name specified not found.".color(ChatColor.RED)
+
+inline fun <reified T : Enum<T>> Executor<*>.enumOrNull(
+        index: Int,
+        argMissing: BaseComponent = MISSING_ENUM_PARAMETER,
+        additionalNames: Map<String, T> = mapOf()
+): T? {
+    val name = string(index, argMissing)
+    return enumValues<T>().find { it.name.equals(name, true) }
+            ?: additionalNames.getIgnoreCase(name)
+}
+
+inline fun <reified T : Enum<T>> Executor<*>.enum(
+        index: Int,
+        argMissing: BaseComponent = MISSING_ENUM_PARAMETER,
+        notFound: BaseComponent = ENUM_VALUE_NOT_FOUND,
+        additionalNames: Map<String, T> = mapOf()
+): T {
+    return enumOrNull(index, argMissing, additionalNames) ?: exception(notFound)
+}
