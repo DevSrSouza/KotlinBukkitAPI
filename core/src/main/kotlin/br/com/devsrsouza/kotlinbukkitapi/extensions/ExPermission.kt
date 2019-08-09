6ad7d0b36636501f131a8344326e1de4f45f1d8e
DevSrSouza <devsrsouza@gmail.com>
2019-08-09 11:24:19 -0300

    permission extension for any, all and permission or star

---
+++ core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/ExPermission.kt
@@ -0,0 +1,12 @@
+package br.com.devsrsouza.kotlinbukkitapi.extensions
+
+import org.bukkit.permissions.Permissible
+
+fun Permissible.anyPermission(vararg permissions: String): Boolean
+        = permissions.any { hasPermission(it) }
+
+fun Permissible.allPermission(vararg permissions: String): Boolean
+        = permissions.all { hasPermission(it) }
+
+fun Permissible.hasPermissionOrStar(permission: String): Boolean
+        = hasPermission(permission) || hasPermission(permission.replaceAfterLast('.', "*"))
