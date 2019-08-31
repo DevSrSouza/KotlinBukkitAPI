9c1ef479b0a499e3b5cf8b9d6f210f348589cc0a
DevSrSouza <devsrsouza@gmail.com>
2019-08-31 11:45:16 -0300

    launchProjectile extension

--- core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/ExEntity.kt
+++ core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/ExEntity.kt
@@ -2,11 +2,10 @@ package br.com.devsrsouza.kotlinbukkitapi.extensions.entity
 
 import br.com.devsrsouza.kotlinbukkitapi.extensions.location.spawn
 import org.bukkit.Location
-import org.bukkit.entity.Entity
-import org.bukkit.entity.EntityType
-import org.bukkit.entity.Firework
-import org.bukkit.entity.Player
+import org.bukkit.entity.*
 import org.bukkit.inventory.meta.FireworkMeta
+import org.bukkit.projectiles.ProjectileSource
+import org.bukkit.util.Vector
 import kotlin.contracts.contract
 
 val Entity.isPlayer: Boolean
@@ -24,3 +23,6 @@ inline fun firework(location: Location, block: FireworkMeta.() -> Unit): Firewor
 inline fun Firework.meta(block: FireworkMeta.() -> Unit) = apply {
     fireworkMeta = fireworkMeta.apply(block)
 }
+
+inline fun <reified T : Projectile> ProjectileSource.launchProjectile() = launchProjectile(T::class.java)
+inline fun <reified T : Projectile> ProjectileSource.launchProjectile(vector: Vector) = launchProjectile(T::class.java, vector)
