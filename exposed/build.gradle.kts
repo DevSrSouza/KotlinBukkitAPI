53d62ec6ef158f0b970ff2c900908aed16065cbb
DevSrSouza <devsrsouza@gmail.com>
2019-10-06 22:42:39 -0300

    removed Item delegate for Exposed (because module server was removed)

--- exposed/build.gradle.kts
+++ exposed/build.gradle.kts
@@ -2,7 +2,6 @@ repositories {}
 
 dependencies {
     compileOnly(project(":core", configuration = "shadow"))
-    compileOnly(project(":server"))
     compileOnly("org.jetbrains.exposed:exposed:0.11.2")
     compileOnly("com.zaxxer:HikariCP:3.3.1")
 }
