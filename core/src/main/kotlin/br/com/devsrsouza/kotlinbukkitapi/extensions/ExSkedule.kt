0010b452162a63f0f0d93b4bf27cf5a5728a5dd8
DevSrSouza <devsrsouza@gmail.com>
2020-01-05 16:59:39 -0200

    Skedule extension for Dispatchers SYNC and ASYNC

--- core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/ExSkedule.kt
+++ core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/ExSkedule.kt
@@ -1,9 +1,12 @@
 package br.com.devsrsouza.kotlinbukkitapi.extensions.skedule
 
 import br.com.devsrsouza.kotlinbukkitapi.extensions.plugin.WithPlugin
+import com.okkero.skedule.BukkitDispatcher
 import com.okkero.skedule.BukkitSchedulerController
 import com.okkero.skedule.SynchronizationContext
 import com.okkero.skedule.schedule
+import org.bukkit.plugin.Plugin
+import org.bukkit.plugin.java.JavaPlugin
 
 fun WithPlugin<*>.schedule(
         initialContext: SynchronizationContext = SynchronizationContext.SYNC,
@@ -15,3 +18,11 @@ val BukkitSchedulerController.contextAsync get() = SynchronizationContext.ASYNC
 
 suspend fun BukkitSchedulerController.switchToSync() = switchContext(contextSync)
 suspend fun BukkitSchedulerController.switchToAsync() = switchContext(contextAsync)
+
+val WithPlugin<*>.BukkitDispatchers get() = JavaPlugin.getProvidingPlugin(plugin::class.java).BukkitDispatchers
+val Plugin.BukkitDispatchers get() = PluginDispatcher(JavaPlugin.getProvidingPlugin(this::class.java))
+
+inline class PluginDispatcher(val plugin: JavaPlugin) {
+    val ASYNC get() = BukkitDispatcher(plugin, true)
+    val SYNC get() = BukkitDispatcher(plugin, false)
+}
