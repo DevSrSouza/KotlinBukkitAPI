7a2df3ac7d97ed1e1e752d8f332045567a86321e
DevSrSouza <devsrsouza@gmail.com>
2020-01-11 13:16:38 -0200

    Utils to take max millisecond in tick with Coroutines

--- core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/ExSkedule.kt
+++ core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/ExSkedule.kt
@@ -1,12 +1,21 @@
 package br.com.devsrsouza.kotlinbukkitapi.extensions.skedule
 
 import br.com.devsrsouza.kotlinbukkitapi.extensions.plugin.WithPlugin
+import br.com.devsrsouza.kotlinbukkitapi.utils.getTakeValuesOrNull
+import br.com.devsrsouza.kotlinbukkitapi.utils.registerCoroutineContextTakes
+import br.com.devsrsouza.kotlinbukkitapi.utils.time.Millisecond
+import br.com.devsrsouza.kotlinbukkitapi.utils.unregisterCoroutineContextTakes
 import com.okkero.skedule.BukkitDispatcher
 import com.okkero.skedule.BukkitSchedulerController
 import com.okkero.skedule.SynchronizationContext
 import com.okkero.skedule.schedule
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
 
 fun WithPlugin<*>.schedule(
         initialContext: SynchronizationContext = SynchronizationContext.SYNC,
@@ -25,4 +34,21 @@ val Plugin.BukkitDispatchers get() = PluginDispatcher(JavaPlugin.getProvidingPlu
 inline class PluginDispatcher(val plugin: JavaPlugin) {
     val ASYNC get() = BukkitDispatcher(plugin, true)
     val SYNC get() = BukkitDispatcher(plugin, false)
+}
+
+// Take max millisecond in a tick
+
+suspend fun BukkitSchedulerController.takeMaxPerTick(time: Millisecond) {
+    val takeValues = getTakeValuesOrNull(context)
+
+    if(takeValues == null) {
+        // registering take max at current millisecond
+        registerCoroutineContextTakes(context, time)
+    } else {
+        // checking if this exceeded the max time of execution
+        if(takeValues.wasTimeExceeded()) {
+            unregisterCoroutineContextTakes(context)
+            waitFor(0)
+        }
+    }
 }
---
+++ core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/utils/TakeMaxTimePerTickUtils.kt
@@ -0,0 +1,54 @@
+package br.com.devsrsouza.kotlinbukkitapi.utils
+
+import br.com.devsrsouza.kotlinbukkitapi.extensions.skedule.BukkitDispatchers
+import br.com.devsrsouza.kotlinbukkitapi.utils.time.Millisecond
+import com.okkero.skedule.BukkitSchedulerController
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import org.bukkit.plugin.Plugin
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
+
+internal val coroutineContextTakes = ConcurrentHashMap<CoroutineContext, TakeValues>()
+internal data class TakeValues(val startTimeMilliseconds: Long, val takeTimeMillisecond: Long) {
+    fun wasTimeExceeded() = System.currentTimeMillis() - startTimeMilliseconds - takeTimeMillisecond >= 0
+}
+
+suspend fun Plugin.takeMaxPerTick(time: Millisecond) {
+    val takeValues = getTakeValuesOrNull(coroutineContext)
+
+    if(takeValues == null) {
+        // registering take max at current millisecond
+        registerCoroutineContextTakes(coroutineContext, time)
+    } else {
+        // checking if this exceeded the max time of execution
+        if(takeValues.wasTimeExceeded()) {
+            unregisterCoroutineContextTakes(coroutineContext)
+            withContext(BukkitDispatchers.SYNC) {
+                // wait next tick using BukkitScheduler
+                delay(1)
+            }
+        }
+    }
+}
+
+internal fun getTakeValuesOrNull(
+        coroutineContext: CoroutineContext
+): TakeValues? = coroutineContextTakes[coroutineContext]
+
+internal fun registerCoroutineContextTakes(
+        coroutineContext: CoroutineContext,
+        time: Millisecond
+) {
+    coroutineContextTakes.put(
+            coroutineContext,
+            TakeValues(System.currentTimeMillis(), time.toMillisecond())
+    )
+}
+
+internal fun unregisterCoroutineContextTakes(
+        coroutineContext: CoroutineContext
+) {
+    coroutineContextTakes.remove(coroutineContext)
+}
