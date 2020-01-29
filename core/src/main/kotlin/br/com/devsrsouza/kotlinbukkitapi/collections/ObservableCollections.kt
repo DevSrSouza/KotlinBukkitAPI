c94f820e3c185f5cd5801891e8be6d618f6e5666
DevSrSouza <devsrsouza@gmail.com>
2020-01-28 21:10:09 -0300

    adding more observable collections

---
+++ core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/collections/ObservableCollections.kt
@@ -0,0 +1,232 @@
+package br.com.devsrsouza.kotlinbukkitapi.collections
+
+typealias ObservableListener = (ObservableAction) -> Unit
+enum class ObservableAction { ADD, SET, REPLACE, REMOVE, CLEAR }
+
+fun <T> observableListOf(mutableList: MutableList<T>) = ObservableList(mutableList)
+fun <T> observableSetOf(mutableSet: MutableSet<T>) = ObservableSet(mutableSet)
+fun <K, V> observableMapOf(mutableMap: MutableMap<K, V>) = ObservableMap(mutableMap)
+
+class ObservableList<T>(
+        private val list: MutableList<T>
+) : MutableList<T> by list, ObservableHolder {
+
+    override val listeners = mutableListOf<ObservableListener>()
+
+    override fun add(element: T): Boolean {
+        runListeners(ObservableAction.ADD)
+        return list.add(element)
+    }
+
+    override fun add(index: Int, element: T) {
+        runListeners(ObservableAction.ADD)
+        return list.add(index, element)
+    }
+
+    override fun addAll(index: Int, elements: Collection<T>): Boolean {
+        runListeners(ObservableAction.ADD)
+        return list.addAll(index, elements)
+    }
+
+    override fun addAll(elements: Collection<T>): Boolean {
+        runListeners(ObservableAction.ADD)
+        return list.addAll(elements)
+    }
+
+    override fun clear() {
+        list.clear()
+        runListeners(ObservableAction.CLEAR)
+    }
+
+    override fun listIterator(): MutableListIterator<T> {
+        return ObservableMutableListIterator(
+                list.listIterator(),
+                ::runListeners
+        )
+    }
+
+    override fun listIterator(index: Int): MutableListIterator<T> {
+        return ObservableMutableListIterator(
+                list.listIterator(index),
+                ::runListeners
+        )
+    }
+
+    override fun remove(element: T): Boolean {
+        return list.remove(element).ifTrue {
+            runListeners(ObservableAction.REMOVE)
+        }
+    }
+
+    override fun removeAll(elements: Collection<T>): Boolean {
+        return list.removeAll(elements).ifTrue {
+            runListeners(ObservableAction.REMOVE)
+        }
+    }
+
+    override fun removeAt(index: Int): T {
+        return list.removeAt(index).apply {
+            runListeners(ObservableAction.REMOVE)
+        }
+    }
+
+    override fun retainAll(elements: Collection<T>): Boolean {
+        return list.retainAll(elements).ifTrue {
+            runListeners(ObservableAction.REMOVE)
+        }
+    }
+
+    override fun set(index: Int, element: T): T {
+        return list.set(index, element).apply {
+            runListeners(ObservableAction.SET)
+        }
+    }
+}
+
+class ObservableMutableListIterator<T>(
+        private val iterator: MutableListIterator<T>,
+        private val runListeners: ObservableListener
+) : MutableListIterator<T> by iterator {
+    override fun add(element: T) {
+        iterator.add(element)
+        runListeners(ObservableAction.ADD)
+    }
+
+    override fun remove() {
+        iterator.remove()
+        runListeners(ObservableAction.REMOVE)
+    }
+
+    override fun set(element: T) {
+        iterator.set(element)
+        runListeners(ObservableAction.SET)
+    }
+}
+
+class ObservableSet<T>(
+        private val set: MutableSet<T>
+) : MutableSet<T> by set, ObservableHolder {
+
+    override val listeners = mutableListOf<ObservableListener>()
+
+    override fun add(element: T): Boolean {
+        return set.add(element).ifTrue {
+            runListeners(ObservableAction.ADD)
+        }
+    }
+
+    override fun addAll(elements: Collection<T>): Boolean {
+        return set.addAll(elements).ifTrue {
+            runListeners(ObservableAction.ADD)
+        }
+    }
+
+    override fun clear() {
+        set.clear()
+        runListeners(ObservableAction.CLEAR)
+    }
+
+    override fun iterator(): MutableIterator<T> {
+        return ObservableMutableIterator(
+                set.iterator(),
+                ::runListeners
+        )
+    }
+
+    override fun remove(element: T): Boolean {
+        return set.remove(element).ifTrue {
+            runListeners(ObservableAction.REMOVE)
+        }
+    }
+
+    override fun removeAll(elements: Collection<T>): Boolean {
+        return set.removeAll(elements).ifTrue {
+            runListeners(ObservableAction.REMOVE)
+        }
+    }
+
+    override fun retainAll(elements: Collection<T>): Boolean {
+        return set.retainAll(elements).ifTrue {
+            runListeners(ObservableAction.REMOVE)
+        }
+    }
+}
+
+class ObservableMutableIterator<T>(
+        private val iterator: MutableIterator<T>,
+        private val runListeners: ObservableListener
+) : MutableIterator<T> by iterator {
+    override fun remove() {
+        iterator.remove()
+        runListeners(ObservableAction.REMOVE)
+    }
+}
+
+class ObservableMap<K, V>(
+        private val map: MutableMap<K, V>
+) : MutableMap<K, V> by map, ObservableHolder {
+
+    override val listeners = mutableListOf<ObservableListener>()
+
+    override fun clear() {
+        map.clear()
+        runListeners(ObservableAction.CLEAR)
+    }
+
+    override fun put(key: K, value: V): V? {
+        return put(key, value).apply {
+            runListeners(ObservableAction.ADD)
+        }
+    }
+
+    override fun putAll(from: Map<out K, V>) {
+        putAll(from)
+        runListeners(ObservableAction.ADD)
+    }
+
+    override fun putIfAbsent(key: K, value: V): V? {
+        return map.putIfAbsent(key, value).also {
+            if(it == null) runListeners(ObservableAction.ADD)
+        }
+    }
+
+    override fun remove(key: K): V? {
+        return map.remove(key)?.also {
+            runListeners(ObservableAction.REMOVE)
+        }
+    }
+
+    override fun remove(key: K, value: V): Boolean {
+        return map.remove(key, value).ifTrue {
+            runListeners(ObservableAction.REMOVE)
+        }
+    }
+
+    override fun replace(key: K, first: V, second: V): Boolean {
+        return map.replace(key, first, second).ifTrue {
+            runListeners(ObservableAction.REPLACE)
+        }
+    }
+
+    override fun replace(key: K, value: V): V? {
+        return map.replace(key, value)?.also {
+            runListeners(ObservableAction.REPLACE)
+        }
+    }
+}
+
+interface ObservableHolder {
+    val listeners: MutableList<ObservableListener>
+
+    fun observe(listener: ObservableListener) {
+        listeners.add(listener)
+    }
+
+    fun runListeners(action: ObservableAction) {
+        for (listener in listeners) {
+            listener(action)
+        }
+    }
+}
+
+private inline fun Boolean.ifTrue(block: () -> Unit) = apply { block() }
