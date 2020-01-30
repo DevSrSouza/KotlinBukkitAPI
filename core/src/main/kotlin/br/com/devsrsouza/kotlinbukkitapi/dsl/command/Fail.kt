834bc193c641be8f058d83960ce8008d139292ea
DevSrSouza <devsrsouza@gmail.com>
2020-01-29 23:04:36 -0300

    adding failIfContains extension for expiration collections to use in Commands

--- core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/dsl/command/Fail.kt
+++ core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/dsl/command/Fail.kt
@@ -1,12 +1,14 @@
 package br.com.devsrsouza.kotlinbukkitapi.dsl.command
 
+import br.com.devsrsouza.kotlinbukkitapi.collections.ExpirationList
+import br.com.devsrsouza.kotlinbukkitapi.collections.ExpirationMap
 import br.com.devsrsouza.kotlinbukkitapi.extensions.text.asText
 import net.md_5.bungee.api.chat.BaseComponent
 
 class CommandFailException(
         val senderMessage: BaseComponent? = null,
         val argMissing: Boolean = false,
-        val execute: () -> Unit = {}
+        inline val execute: () -> Unit = {}
 ) : RuntimeException()
 
 inline fun Executor<*>.fail(
@@ -17,9 +19,29 @@ inline fun Executor<*>.fail(
 inline fun Executor<*>.fail(
         senderMessage: String = "",
         noinline execute: () -> Unit = {}
-): Nothing = throw CommandFailException(senderMessage.takeIf { it.isNotEmpty() }?.asText(), execute = execute)
+): Nothing = fail(senderMessage.takeIf { it.isNotEmpty() }?.asText(), execute = execute)
 
 inline fun Executor<*>.fail(
         senderMessage: List<String> = listOf(),
         noinline execute: () -> Unit = {}
-): Nothing = throw CommandFailException(senderMessage.takeIf { it.isNotEmpty() }?.asText(), execute = execute)
+): Nothing = fail(senderMessage.takeIf { it.isNotEmpty() }?.asText(), execute = execute)
+
+// expiration collections
+
+inline fun <T> ExpirationList<T>.failIfContains(
+        element: T,
+        execute: (missingTime: Int) -> Unit
+) {
+    missingTime(element)?.let(execute)?.run {
+        throw CommandFailException()
+    }
+}
+
+inline fun <K> ExpirationMap<K, *>.failIfContains(
+        key: K,
+        execute: (missingTime: Long) -> Unit
+) {
+    missingTime(key)?.let(execute)?.run {
+        throw CommandFailException()
+    }
+}
