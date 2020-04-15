307c78f7fb1d671d1359347bc512609aa697c373
Gabriel Souza <devsrsouza@gmail.com>
2020-04-15 17:43:32 -0300

    fix lifecycle based coroutine scope

--- architecture/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/architecture/lifecycle/CoroutineLifecycle.kt
+++ architecture/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/architecture/lifecycle/CoroutineLifecycle.kt
@@ -5,32 +5,16 @@ import br.com.devsrsouza.kotlinbukkitapi.extensions.skedule.BukkitDispatchers
 import kotlinx.coroutines.*
 
 /**
- * A CoroutineScope that trigger in the Main Thread of Bukkit by default
- *
- * This scope ensures that your task will complete before the plugin disable doing
- * a [Job.join] in all your active jobs.
- *
- * NOTE: If you have a infinity task this will stop the plugin from disable,
- * even could block the server, if you have a repeating task that never ends
- * you should use [cancellableCoroutineScope].
- */
-val LifecycleListener<*>.coroutineScope: CoroutineScope
-    get() = plugin.coroutineScope
-
-val KotlinPlugin.coroutineScope: CoroutineScope
-    get() = getOrInsertCoroutineLifecycle().coroutineScope
-
-/**
- * A CoroutineScope that trigger in the Main Thread of Bukkit by default
+ * A CoroutineScope that trigger in the Main Thread of Bukkit by default.
  *
  * This scope ensures that your task will be canceled when the plugin disable
  * removing the possibility of Job leaks.
  */
-val LifecycleListener<*>.cancellableCoroutineScope: CoroutineScope
-    get() = plugin.cancellableCoroutineScope
+val LifecycleListener<*>.pluginCoroutineScope: CoroutineScope
+    get() = plugin.pluginCoroutineScope
 
-val KotlinPlugin.cancellableCoroutineScope: CoroutineScope
-    get() = getOrInsertCoroutineLifecycle().cancellableCoroutineScope
+val KotlinPlugin.pluginCoroutineScope: CoroutineScope
+    get() = getOrInsertCoroutineLifecycle().pluginCoroutineScope
 
 private fun KotlinPlugin.getOrInsertCoroutineLifecycle(): CoroutineLifecycle {
     return getOrInsertGenericLifecycle(Int.MIN_VALUE) {
@@ -43,21 +27,12 @@ internal class CoroutineLifecycle(
 ) : LifecycleListener<KotlinPlugin> {
 
     private val job = Job()
-    private val cancelableJob = Job()
 
-    val coroutineScope = CoroutineScope(BukkitDispatchers.SYNC + job)
-    val cancellableCoroutineScope = CoroutineScope(BukkitDispatchers.SYNC + cancelableJob)
+    val pluginCoroutineScope = CoroutineScope(BukkitDispatchers.SYNC + job)
 
     override fun onPluginEnable() {}
 
     override fun onPluginDisable() {
-        if(cancelableJob.isActive)
-            cancelableJob.cancel()
-
-        if(job.isActive) {
-            runBlocking {
-                job.join()
-            }
-        }
+        job.cancel()
     }
 }
