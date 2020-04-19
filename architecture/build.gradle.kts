59da3d6cb382b71cef93a9e93f491d4aa056d5b0
Gabriel Souza <devsrsouza@gmail.com>
2020-04-19 00:12:41 -0300

    fix gradle build problem with architecture module

--- architecture/build.gradle.kts
+++ architecture/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     compileOnly(project(":core", configuration = "shadow"))
-    compileOnly(project(":serialization"))
+    compileOnly(project(":serialization", configuration = "shadow"))
 
     compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
 }
