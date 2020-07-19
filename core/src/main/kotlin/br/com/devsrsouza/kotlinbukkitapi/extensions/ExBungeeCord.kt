a9e30d5afe932d7ab4d9ffe2e7a6977356edcef8
DevSrSouza <devsrsouza@gmail.com>
2020-07-19 00:08:30 -0300

    adding coroutines extension to ExBungeecord

--- core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/ExBungeeCord.kt
+++ core/src/main/kotlin/br/com/devsrsouza/kotlinbukkitapi/extensions/ExBungeeCord.kt
@@ -1,14 +1,10 @@
 package br.com.devsrsouza.kotlinbukkitapi.extensions.bungeecord
 
-import br.com.devsrsouza.kotlinbukkitapi.KotlinBukkitAPI
 import br.com.devsrsouza.kotlinbukkitapi.controllers.provideBungeeCordController
-import br.com.devsrsouza.kotlinbukkitapi.provideKotlinBukkitAPI
-import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import com.google.common.io.ByteStreams
-import org.bukkit.plugin.messaging.PluginMessageListener
-import java.nio.ByteBuffer
-import java.nio.charset.Charset
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 val Player.bungeecord get() = BungeeCord(this)
 
@@ -16,18 +12,20 @@ fun Player.sendBungeeCord(message: ByteArray) = provideBungeeCordController().se
 
 typealias ResponseCallback = (message: ByteArray) -> Unit
 
-class BungeeCordRequest(val player: Player,
-                        val subChannel: String,
-                        val request: ByteArray? = null,
-                        val responseCallback: ResponseCallback? = null) {
+class BungeeCordRequest(
+        val player: Player,
+        val subChannel: String,
+        val request: ByteArray? = null,
+        val responseCallback: ResponseCallback? = null
+) {
     fun send() {
         val out = ByteStreams.newDataOutput()
         out.writeUTF(subChannel)
-        if(request != null) out.write(request)
+        if (request != null) out.write(request)
 
         player.sendBungeeCord(out.toByteArray())
 
-        if(responseCallback != null) provideBungeeCordController().addToQueue(this)
+        if (responseCallback != null) provideBungeeCordController().addToQueue(this)
     }
 }
 
@@ -39,13 +37,16 @@ inline class BungeeCord(private val player: Player) {
             ByteStreams.newDataOutput().apply{writeUTF(server)}.toByteArray()
     ).send()
 
-    fun ip(callback: (adress: String, port: Int) -> Unit) = BungeeCordRequest(player, "IP") {
+    fun ip(callback: (address: String, port: Int) -> Unit) = BungeeCordRequest(player, "IP") {
         val input = ByteStreams.newDataInput(it)
         val ip = input.readUTF()
         val port = input.readInt()
         callback(ip, port)
     }.send()
 
+    suspend fun ip(): Pair<String, Int> = suspendCoroutine {  continuation ->
+        ip { address, port ->  continuation.resume(address to port) }
+    }
 
     fun online(server: String = "ALL", callback: (playerCount: Int) -> Unit)
             = BungeeCordRequest(player, "PlayerCount", ByteStreams.newDataOutput().apply{writeUTF(server)}.toByteArray()) {
@@ -55,11 +56,19 @@ inline class BungeeCord(private val player: Player) {
         callback(playerCount)
     }.send()
 
+    suspend fun online(server: String = "ALL"): Int = suspendCoroutine { continuation ->
+        online {  continuation.resume(it) }
+    }
+
     fun getServerName(callback: (server: String) -> Unit) = BungeeCordRequest(player, "GetServer") {
         val input = ByteStreams.newDataInput(it)
         val server = input.readUTF()
         callback(server)
     }.send()
+
+    suspend fun getServerName(): String = suspendCoroutine { continuation ->
+        getServerName {  continuation.resume(it) }
+    }
 
     fun kickPlayer(player: String, reason: String) = BungeeCordRequest(this.player, "KickPlayer",
         ByteStreams.newDataOutput().apply{writeUTF(player);writeUTF(reason)}.toByteArray()).send()
