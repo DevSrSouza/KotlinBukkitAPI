8ab4a73fe3336d18246871aca85739d18c37703a
DevSrSouza <devsrsouza@gmail.com>
2020-09-16 23:26:53 -0300

    updating kaml version to the 1.4.0 supported

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -22,7 +22,7 @@ object Dep {
         val hikari = "3.3.1"
         val bstats = "1.7"
         val spigotApi = "1.8.8-R0.1-SNAPSHOT"
-        val kaml = "0.19.0"
+        val kaml = "0.22.0"
         val corouties = "1.3.9"
         val skedule = "1.2.6"
         val shadow = "6.0.0"
